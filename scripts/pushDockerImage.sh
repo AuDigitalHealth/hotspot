75d55410a406c4f01c54ddc03c9803ac1efa20f3
John Grimes <John.Grimes@digitalhealth.gov.au>
2018-05-17 20:20:09 +1000

    Simplify Docker push script

--- scripts/pushDockerImage.sh
+++ scripts/pushDockerImage.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 set -e
-if [[ -v DOCKER_USER && -v DOCKER_PASSWORD ]]; then
-  docker login -u $DOCKER_USER -p $DOCKER_PASSWORD
-fi
+
+docker login -u $DOCKER_USER -p $DOCKER_PASSWORD
 docker push $DOCKER_IMAGE
-if [[ -v DOCKER_USER && -v DOCKER_PASSWORD ]]; then
-  docker logout
-fi
+docker logout
